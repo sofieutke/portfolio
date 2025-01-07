9bf54c948a45fc96b148f6edc3c9b11a7769f13d
sofieutke <sobo0001@stud.kea.dk>
2025-01-08 00:04:30 +0100

    done

--- login.js
+++ login.js
@@ -1,36 +1,44 @@
-// Funktion til at toggle burgermenuen og åbne overlay
+// lytter efter at HTML-dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", function () {
+  // finder elementet og gemmer i variabel
   const burger = document.querySelector(".burger");
   const overlay = document.getElementById("myNav");
 
+  // tilføjer klik
   burger.addEventListener("click", function () {
-    // Toggler en 'change' klasse til at animere burgermenuen
+    // skifter class til change
     burger.classList.toggle("change");
-    // Skifter overlayets bredde afhængigt af dens nuværende tilstand
+
+    // Tjekker om bredde er 100%
     if (overlay.style.width === "100%") {
+      // skjuler overlay
       overlay.style.width = "0";
     } else {
+      // viser overlay
       overlay.style.width = "100%";
     }
   });
 
-  // Tilføj click event listener til close-knappen inde i overlay
+  // lytter til klik til lukkeknappen
   document.querySelector(".closebtn").addEventListener("click", function () {
     overlay.style.width = "0";
   });
 });
 
-/************ accordion på OM SOFIE siden */
+//********* Accordion på OM SOFIE siden ********* //
+
+// finder elementet og gemmer i variabel
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+// går igennem alle accordion elementer
 for (i = 0; i < acc.length; i++) {
+  // tilføjer klik
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+    // tilføjer og fjerner klassen "active"
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
+    // skjuler og viser det aktive panel
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
